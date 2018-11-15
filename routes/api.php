671ab213f978c60340c385ba3226d371602413a8
Rafael Nogueira <rafaelnogueira@localhost.localdomain>
2018-11-15 18:22:43 -0300

    corrigindo configurações da heroku

--- routes/api.php
+++ routes/api.php
@@ -20,22 +20,22 @@ ApiRoute::version('v1',function(){
         'middleware' => ['bindings']
     ], function(){
         ApiRoute::post('/access_token', [
-            'uses' => 'AuthController@accessToken',
+            'uses' => 'AuthController@accessToken'/*,
             'middleware' => ['api.throttle'],
             'limit' => 100,
-            'expires' => 100
+            'expires' => 100*/
         ])->name('.access_token');
 
         ApiRoute::post('/refresh_token', [
-            'uses' => 'AuthController@refreshToken',
+            'uses' => 'AuthController@refreshToken'/*,
             'middleware' => 'api.throttle',
             'limit' => 10,
-            'expires' => 1
+            'expires' => 1*/
         ])->name('.refresh_token');
 
         ApiRoute::post('/register', 'RegisterUsersController@store');
 
-        ApiRoute::group(['middleware' => ['api.throttle', 'api.auth']
+        ApiRoute::group(['middleware' => [/*'api.throttle',*/ 'api.auth']
             , 'limit' => 100
             , 'expires' =>3
         ], function(){
