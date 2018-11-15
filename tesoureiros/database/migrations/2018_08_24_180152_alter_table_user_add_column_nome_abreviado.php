12c956dc5f368ca821cad535d8cd98df80207e5f
Rafael Nogueira <rafaelnogueira@localhost.localdomain>
2018-11-15 17:31:20 -0300

    Alterando o migration

--- tesoureiros/database/migrations/2018_08_24_180152_alter_table_user_add_column_nome_abreviado.php
+++ tesoureiros/database/migrations/2018_08_24_180152_alter_table_user_add_column_nome_abreviado.php
@@ -14,7 +14,7 @@ class AlterTableUserAddColumnNomeAbreviado extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('nome_abreviado')->nullable();
+            //$table->text('nome_abreviado')->nullable();
         });
     }
 
