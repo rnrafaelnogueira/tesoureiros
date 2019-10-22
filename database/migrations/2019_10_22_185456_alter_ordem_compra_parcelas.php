93e6e7558fe5aaebeff8d13613fc9adb5b09b25a
Rafael da Silva Nogueira <rsnogueira@sfiec.org.br>
2019-10-22 15:59:29 -0300

    Alteração na tabela de order de compra parcelas

---
+++ database/migrations/2019_10_22_185456_alter_ordem_compra_parcelas.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterOrdemCompraParcelas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('ordem_compra_parcelas', function (Blueprint $table) {
+            $table->text('id_ordem_compra');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
