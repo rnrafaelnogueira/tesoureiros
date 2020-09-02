d76e2870df8803c5204d6576e5d61a8560178350
Rafael Nogueira <rafaelnogueira@localhost.localdomain>
2020-09-02 17:58:16 -0300

    Montando ambiente

--- server.php
+++ server.php
@@ -18,5 +18,4 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-//require_once __DIR__.'/public/index.php';
-require_once __DIR__.'/public/site/index.html';
+require_once __DIR__.'/public/index.php';
